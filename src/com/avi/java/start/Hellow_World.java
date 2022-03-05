c4d17503f17265baf565653c91ad6064733fc14f
dhumalavi <dhumalavin@gmail.com>
2022-03-05 15:24:31 +0530

    initial commit from my new dell 3511

---
+++ src/com/avi/java/start/Hellow_World.java
@@ -0,0 +1,13 @@
+package com.avi.java.start;
+
+public class Hellow_World 
+{
+
+	public static void main(String []args)
+	{
+		System.out.println("Hellow World");
+		System.out.println("Hellow World 2nd time");
+		System.out.println("Hellow World 3nd time");
+
+	}
+}
